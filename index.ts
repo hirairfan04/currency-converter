5ef459026eb924e6b4ba5ea4543360cdc82c8192
Hira Irfan <162793914+hirairfan04@users.noreply.github.com>
2024-04-06 15:52:59 +0500

    program files

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const fixed_currency: any={
+    USD: 1,
+    PKR: 277.54,
+    INR: 83.30,
+    CNY: 7.23,
+    AFN: 71.23,
+    IRR: 42075,
+    KRW: 1351.27
+}
+
+let user_input= await inquirer.prompt(
+  [  {
+        message:"From currency",
+        name: "from",
+        type: "list",
+        choices: ['USD','PKR','INR','CNY','AFN','IRR','KRW']
+    },
+    {
+        message:"To currency",
+        name: "to",
+        type: "list",
+        choices: ['USD','PKR','INR','CNY','AFN','IRR','KRW']
+    },
+    {
+        message: "Enter amount",
+        name: "amount",
+        type: "number"
+    }
+]
+)
+
+let fromAmount= fixed_currency [user_input.from]
+let toAmount= fixed_currency [user_input.to]
+let amt= user_input.amount
+let baseAmount= amt/fromAmount
+let convertedAmount= baseAmount*toAmount
+console.log(convertedAmount.toFixed(2))
